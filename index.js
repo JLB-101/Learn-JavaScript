c8e54c1dd66cb49de58f2cc16a2d0d0c51fb3ab5
Julião LB <juliaolangassitela@gmail.com>
2024-01-26 01:39:38 +0200

    Nesting For Loops

--- index.js
+++ index.js
@@ -860,3 +860,18 @@ for (var i = 0; i < myArr.length; i++) {
  }
  console.log(total);
  
+//  Nesting For Loops
+function multiplyAll(arr) {
+    var product = 1;
+    for (var i = 0; i < arr.length; i++) {
+        for (var j=0; j < arr[i].length; j++) {
+            product *= arr[i][j];
+        }
+   }
+    return product;
+}
+
+var product = multiplyAll([[1,2], [3,4], [5,6,7]]);
+
+console.log(product);
+
