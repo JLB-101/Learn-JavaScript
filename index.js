ec238c7a475d147e3ea7fbb16501cf44fcb71631
Julião LB <juliaolangassitela@gmail.com>
2024-01-26 01:47:10 +0200

    Generate Random wholw Number

--- index.js
+++ index.js
@@ -940,3 +940,13 @@ function randomFraction() {
     return Math.random();
 }
 console.log(randomFraction);
+
+// Generate Random wholw Number
+ 
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+ 
+function randomWholeNum() {
+    
+    return Math.floor(Math.random() * 10);
+}
+console.log(randomWholeNum());
