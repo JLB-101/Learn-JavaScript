21978a1c53a8bec06b9376cb83df26967bf49e1c
Julião LB <juliaolangassitela@gmail.com>
2024-01-28 22:55:52 +0200

    Mutatie an Array Declared with const

--- index.js
+++ index.js
@@ -1046,4 +1046,16 @@ function printManyTimes(str) {
     }
     
 }
-printManyTimes("FreeCodeCamp");
+printManyTimes("FreeCodeCamp"); 
+
+// Mutatie an Array Declared with const
+const s = [5,7,2];
+function editInPlace() {
+    "use strict";
+    //s = [2,5,7];
+    s[0] = 2;
+    s[1] = 5;
+    s[2] = 7;
+} 
+editInPlace();
+console.log(s);
