8f967c671be23019da208ec7c33caf3d7b50cdce
Julião LB <juliaolangassitela@gmail.com>
2024-01-29 00:43:25 +0200

    Use Destructuring Assignment with the Rest Operator

--- index.js
+++ index.js
@@ -1227,3 +1227,22 @@ function removeFirstTwo(list) {
 const arr = removeFirstTwo(source);
 console.log(arr);
 console.log(source);
+
+// Use Destructuring Assignment with the Rest Operator
+
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+const half = (function () {
+    return function half({max, min}){
+        //return(stats.max + stats.min)/2.0;
+        return(max + min)/2.0;
+    };
+})();
+console.log(stats);
+console.log(half(stats));
