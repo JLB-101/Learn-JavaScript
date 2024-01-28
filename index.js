89ee8cda0f1878c9054fcbdc61e536f72ce599b9
Julião LB <juliaolangassitela@gmail.com>
2024-01-28 23:50:03 +0200

    Write Higher Order Arrow Functions

--- index.js
+++ index.js
@@ -1124,3 +1124,14 @@ const increment = (function()  {
 
 console.log(increment(5,2));
 console.log(increment(5));
+
+// Write Higher Order Arrow Functions 
+
+const sum = (function()  {
+    return function sum(x, y, z){
+        const args = [x, y, z];
+        return args.reduce((a,b) => a+b, 0);
+    };
+})();
+
+console.log(sum(1,2,3));
