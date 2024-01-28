9f32570d81a2ad4fb3717505135fd48066a01679
Julião LB <juliaolangassitela@gmail.com>
2024-01-29 00:18:58 +0200

    Use Destructuring Assignment to Assign Variables from Objects

--- index.js
+++ index.js
@@ -1155,3 +1155,30 @@ let arr2;
     arr1[0] = 'potato';
 })();
 console.log(arr2);
+
+/* USe Destructuring Assignment to Assign Variables from Objects*/
+var voxel = { x:3.6, y: 7.4, z:6.54 };
+var x = voxel.x;// x = 3.6
+var y = voxel.y;// y = 7.4
+var z = voxel.z;// z = 6.54
+
+const { x : a, y : b, z : c } = voxel;// a = 3.6, b = 7.4, c = 6.54
+
+const AVG_TEMPERATURES = {
+    today: 77.5,
+    tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+    "use strict";
+    // change code below this line
+    //const tempOfTomorrow = undefined; //change this line 
+    const {tomorrow : tempOfTomorrow } = avgTemperatures;
+    // to this line
+    
+    // change above this line
+    return tempOfTomorrow;
+    
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79 
