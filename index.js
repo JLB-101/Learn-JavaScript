f0b9249614ece999edd993f92d12bf3abb2a8af0
Julião LB <juliaolangassitela@gmail.com>
2024-01-28 23:46:47 +0200

    Write Higher Order Arrow Functions

--- index.js
+++ index.js
@@ -1103,7 +1103,7 @@ var magic = () => new Date();
 var myConcat = (arr1, arr2) => arr1.concat(arr2);
 console.log(myConcat([1,2], [3, 4, 5]));
 
-// Write Higher Order Arrow Functions 
+//   
 var realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
 const squareList = (arr) => {
     const squaredIntegers = arr.filter(num => number.isInteger(num) && num> 0).map(x => x * x);
@@ -1112,3 +1112,15 @@ const squareList = (arr) => {
 
 const squaredIntegers = squareList(realNumberArray);
 console.log(squaredIntegers);
+
+
+// Write Higher Order Arrow Functions 
+
+const increment = (function()  {
+    return function increment(number, value = 1){
+        return number + value;
+    }   
+})();
+
+console.log(increment(5,2));
+console.log(increment(5));
