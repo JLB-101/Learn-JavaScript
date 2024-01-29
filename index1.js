53ed2f0621890126f48f7efe0587a1399994cfeb
Julião LB <juliaolangassitela@gmail.com>
2024-01-30 00:18:51 +0200

    Create an Export Fallback with export default

--- index1.js
+++ index1.js
@@ -1,2 +1,7 @@
 // Use * to Import Everything from a file
 import * as capitalizeString from "capitalize_strings";
+
+
+//Create an Export Fallback with export default
+//export default function subtract(x,y) {return x-y;} // "maths_functions"
+
