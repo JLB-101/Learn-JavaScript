23dacfdc8baa48ef5dd0a7f5064fe3ea4caed6ae
Julião LB <juliaolangassitela@gmail.com>
2024-01-30 00:20:39 +0200

    Import a Default Export

--- index1.js
+++ index1.js
@@ -5,3 +5,6 @@ import * as capitalizeString from "capitalize_strings";
 //Create an Export Fallback with export default
 //export default function subtract(x,y) {return x-y;} // "maths_functions"
 
+// Import a Default Export
+import subtract from "maths_functions";
+console.log(subtract(7,4));
