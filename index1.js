08a7d3b2c8cb0c1fa5b1ade933a79d51f8f991a6
Julião LB <juliaolangassitela@gmail.com>
2024-01-30 00:29:29 +0200

    fixing Error: Unknown require file

--- index1.js
+++ index1.js
@@ -1,10 +1,10 @@
 // Use * to Import Everything from a file
-import * as capitalizeString from "capitalize_strings";
+import * as capitalizeString from "./capitalize_strings";
 
 
 //Create an Export Fallback with export default
 //export default function subtract(x,y) {return x-y;} // "maths_functions"
 
 // Import a Default Export
-import subtract from "maths_functions";
+import subtract from "./maths_functions";
 console.log(subtract(7,4));
